a710d2313d0695eef1f9691d2c2427a14e5cea5b
Ngduchoa <hoandph50532@gmail.com>
2024-09-26 01:10:35 +0700

    add grade_point

--- my_package/grade_point.py
+++ my_package/grade_point.py
@@ -9,17 +9,82 @@ Bao gồm các thuộc tính:
 - Phân loại: Loại của đầu điểm (ví dụ: lý thuyết, thực hành, v.v.)
 ------------------------------------"""
 
-class GradePoint:
-    def __init__(self, grade_point_id, name, weight, subject, classification):
-        """
-        grade_point_id: mã đầu điểm
-        name: tên đầu điểm
-        weight: trọng số của đầu điểm
-        subject: đối tượng Môn học
-        classification: phân loại đầu điểm
-        """
-        self.grade_point_id = grade_point_id
-        self.name = name
-        self.weight = weight
-        self.subject = subject
-        self.classification = classification
+
+def hien_thi_danh_sach():
+    print("Thực hiện hiển thị danh sách Đầu điểm")
+
+def Them_vao_dau_diem():
+    print("Thực hiện thêm học sinh")
+
+def Xoa_Dau_diem():
+    print("Thực hiện lệnh xóa Đầu điểm") 
+
+def Cap_nhat_dau_diem():
+    print("Thực hiện cập nhật Đầu điểm")
+
+def Tim_kiem_Loc():
+    print("Thực hiện tìm kiếm / lọc")
+
+def Kiem_tra_dau_diem():
+    print('Thực hiện kiểm tra đầu điểm nào có điểm số cao nhất') 
+
+def Thong_ke():
+    print("Thực hiện thống kê số lượng đầu điểm theo từng môn học")    
+
+def kiem_tra():
+    print("Kiểm tra sự tồn tại của mã đầu điểm")
+
+def kiem_tra_hop_le():
+    print("Kiểm tra tính hợp lệ (đủ, thiếu, thừa)")
+
+def Xuat_file():
+    print("Xuất file theo môn")
+
+
+while True:
+    print("-----------Đầu-Điểm-----------")
+    print("|1. Hiển thị danh sách       |")
+    print("|2. Thêm vào đầu điểm        |")
+    print("|3. Xóa đầu điểm             |")
+    print("|4. Cập nhật đầu điểm        |")
+    print("|5. Tìm kiếm / Lọc           |")
+    print("|6. Kiểm tra đầu điểm        |")
+    print("|7. Kiểm tra mã đầu điểm     |")
+    print("|8. Thống kê                 |")
+    print("|9. Kiểm tra tính hợp lệ     |")
+    print("|10. Xuất file theo môn      |")
+    print("|0. Kết thúc chương trình    |")
+    print("------------------------------")
+
+    try:
+        swap = int(input("Vui lòng chọn chương trình: "))
+    except ValueError:
+        print("Vui lòng nhập một số nguyên.")
+        continue
+
+    if swap == 0:
+        print("Kết thúc chương trình.")
+        break
+    elif swap == 1:
+        hien_thi_danh_sach()
+    elif swap == 2:
+        Them_vao_dau_diem()
+    elif swap == 3:
+        Xoa_Dau_diem()
+    elif swap == 4:
+        Cap_nhat_dau_diem()
+    elif swap == 5:
+        Tim_kiem_Loc()
+    elif swap == 6:
+        Kiem_tra_dau_diem()
+    elif swap == 7:
+        kiem_tra()
+    elif swap == 8:
+        Thong_ke()
+    elif swap == 9:
+        kiem_tra_hop_le()
+    elif swap == 10:
+        Xuat_file()
+    
+    else:
+        print("Lựa chọn không hợp lệ, vui lòng thử lại.")
